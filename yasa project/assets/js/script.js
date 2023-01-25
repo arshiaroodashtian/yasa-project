a049ad78f373699b613d142217a7f8074176d6b4
arshiaroodashtian <123540161+arshiaroodashtian@users.noreply.github.com>
2023-01-25 11:16:10 +0330

    Add files via upload

---
+++ yasa project/assets/js/script.js
@@ -0,0 +1,64 @@
+var rqjs = document.getElementById('requirejs');
+requirejs.config({
+	baseUrl: rqjs.getAttribute('src').replace('require.js',''),
+	paths: {
+		mmenu: 'mmenu-light',
+	}
+});
+if (typeof jQuery === 'function') define('jQuery', function () {
+	return jQuery;
+});
+const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+	entries.forEach(function(entry){
+		if (entry.isIntersecting){
+			const lazyImage = entry.target;
+			lazyImage.src = lazyImage.dataset.src;
+			imgObserver.unobserve(lazyImage);
+		}
+	});
+});
+
+jQuery(document).ready(function($){
+	"use strict";
+
+	function init_scripts(){
+		const lazyImages = document.querySelectorAll('[data-src]');
+		for (var i = 0; i < lazyImages.length; i++) {
+			imageObserver.observe(lazyImages[i]);
+		}
+
+		requirejs(['bootstrap'], function(bootstrap){
+			const tooltipList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+			for ( var i = 0; i < tooltipList.length; i++ ) {
+				new bootstrap.Tooltip(tooltipList[i]);
+			}
+		});
+
+		if(document.getElementsByClassName('splide')){
+			requirejs(['splide'], function( Splide ){
+				const splides = document.getElementsByClassName('splide');
+				for ( var i = 0; i < splides.length; i++ ) {
+					new Splide(splides[i]).mount();
+				}
+			});
+		}
+	}
+	init_scripts();
+	if(document.getElementById('mmenu')){
+		requirejs(['mmenu'], function(){
+			const menu = new MmenuLight(document.getElementById('mmenu')), drawer = menu.offcanvas({position: ((document.dir == 'rtl') ? 'right' : 'left')});
+			menu.navigation({title: $('#mmenu').data('mm-spn-title')});
+			$('[href="#mmenu"]').click(function (e) { 
+				e.preventDefault();
+				drawer.open();
+			});
+		});
+	}
+
+	$('.widget .categories li>a').click(function(e){
+		if(($this).parent().has('ul').length){
+			e.preventDefault();
+			$(this).parent().find('ul').slidetoggle();
+		}
+	});
+}); 
